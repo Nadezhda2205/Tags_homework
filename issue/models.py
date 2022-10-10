22ddc07665bf7d19fb294968f32a594b6ff9b980
Nadezhda Khovrich <hope28281@gmail.com>
2022-10-10 22:01:23 +0600

    Создана модель Tags связь многие ко многим

--- issue/models.py
+++ issue/models.py
@@ -15,6 +15,14 @@ class Type(models.Model):
         return self.name
 
 
+
+class Tag(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=50, null=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Task(models.Model):
     summary = models.CharField(verbose_name='Заголовок', max_length=200, null=False)
     description = models.TextField(verbose_name='Описание', null=True)
@@ -22,6 +30,8 @@ class Task(models.Model):
     type = models.ForeignKey(to='issue.Type', verbose_name='Тип', related_name='type', on_delete=models.RESTRICT)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    tags = models.ManyToManyField(to=Tag, verbose_name='Тэги', blank=True, related_name='tasks', null=True)
 
     def __str__(self) -> str:
         return self.summary
+
